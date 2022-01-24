c8a1354b7200d74eb36a968176668d06ef8b7aa3
PriyanKishoreMS <priyankishore2000@gmail.com>
2022-01-24 23:47:53 +0530

    beta

--- bot.py
+++ bot.py
@@ -2,16 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
+import datetime
 from cred import email, password
 
+from timetable import timetable, tim
+
 opt = Options()
-prefs = {"credentials_enable_service": False,
-         "profile.password_manager_enabled": False}
 opt.add_argument("--disable-infobars")
 opt.add_argument("--disable-extensions")
 opt.add_argument("--start-maximized")
-opt.add_experimental_option("prefs", prefs)
+# Pass the argument 1 to allow and 2 to block
+opt.add_experimental_option("prefs", {
+    "credentials_enable_service": False,
+    "profile.password_manager_enabled": False,
+    "profile.default_content_setting_values.media_stream_mic": 1,
+    "profile.default_content_setting_values.media_stream_camera": 1,
+    "profile.default_content_setting_values.geolocation": 1,
+    "profile.default_content_setting_values.notifications": 1
+})
 opt.add_experimental_option("excludeSwitches", ['enable-automation'])
+
 driver = webdriver.Chrome(options=opt)
 URL = "https://teams.microsoft.com/"
 driver.get(URL)
@@ -24,13 +34,119 @@ def login():
     emailField.click()
     emailField.send_keys(email)
     driver.find_element(By.XPATH, '//input[@id="idSIButton9"]').click()
+    print("Email entered")
     time.sleep(3)
     passwd = driver.find_element(By.XPATH, '//input[@name="passwd"]')
     passwd.click()
     passwd.send_keys(password)
     driver.find_element(By.XPATH, '//input[@id="idSIButton9"]').click()
+    print("Password Entered")
     time.sleep(3)
     driver.find_element(By.XPATH, '//input[@id="idSIButton9"]').click()
 
 
+def join(cls, now):
+    try:
+        joinbtn = driver.find_element(
+            By.XPATH, '//button[@title="Join call with video"]')
+        joinbtn.click()
+
+    except:
+        i = 1
+        while(i <= 15):
+            print("Meeting hasn't started yet, trying again")
+            time.sleep(60)
+            driver.refresh()
+            join(cls, now)
+            i += 1
+            print(f"{cls} is not scheduled today!")
+
+    else:
+        time.sleep(4)
+        webcam = driver.find_element(
+            By.XPATH, '//*[@id="page-content-wrapper"]/div[1]/div/calling-pre-join-screen/div/div/div[2]/div[1]/div[2]/div/div/section/div[2]/toggle-button[1]/div/button/span[1]')
+        if(webcam.get_attribute('title') == 'Turn camera off'):
+            webcam.click()
+        time.sleep(1)
+
+        microphone = driver.find_element(By.XPATH,
+                                         '//*[@id="preJoinAudioButton"]/div/button/span[1]')
+        if(microphone.get_attribute('title') == 'Mute microphone'):
+            microphone.click()
+
+        time.sleep(1)
+        joinnowbtn = driver.find_element(By.XPATH,
+                                         '//*[@id="page-content-wrapper"]/div[1]/div/calling-pre-join-screen/div/div/div[2]/div[1]/div[2]/div/div/section/div[1]/div/div/button')
+        joinnowbtn.click()
+        print(f"joined class: {cls} at {now}")
+
+
+def jointest(cls, now):
+    print(f"Joined cls: {cls} at {now}")
+
+
+def leave(cls):
+    teamsbtn = driver.find_element(
+        By.XPATH, '//button[@id="app-bar-2a84919f-59d8-4441-a975-2a8c2643b741"]')
+    try:
+        driver.find_element(By.XPATH, '//button[@id="hangup-button"]').click()
+        print(f"Left from: {cls}")
+        teamsbtn.click()
+        teamsbtn.click()
+
+    except:
+        print(f"Left from: {cls}")
+        teamsbtn.click()
+        teamsbtn.click()
+
+
+def selectClass():
+    today = datetime.datetime.now().strftime("%A")
+    print("Logged in")
+    time.sleep(20)
+    now = datetime.datetime.now().strftime("%0H.%M")
+    # now = "14.42"
+    count = 0
+    for i in tim:
+        count = count+1
+        if now <= i:
+            break
+    for cls in timetable[today][count-2:]:
+        now = datetime.datetime.now().strftime("%0H.%M")
+        # now = "14.42"
+        count = 0
+        for i in tim:
+            count = count+1
+            if now <= i:
+                break
+
+        format = '%H.%M'
+        timer = datetime.datetime.strptime(
+            tim[count-1], format) - datetime.datetime.strptime(now, format)
+        # print(timer)
+        ftime = str(timer)
+        stime = (int(ftime[2:4]))
+        ftime = stime * 60
+        print(f"Class time duration: {stime}min / {ftime}sec")
+
+        if cls == "free":
+            print("\nFree Class\n")
+            time.sleep(ftime)
+        elif cls == "break":
+            print("\n20 mins Break\n")
+            time.sleep(ftime)
+        else:
+            driver.find_element(
+                By.XPATH, f'//div[@aria-label="{cls}"]').click()
+            time.sleep(20)
+            jointest(cls, now)
+            print(f"Attending class for the next {stime}mins")
+            time.sleep(ftime)
+            leave(cls)
+            # driver.find_element(
+            #     By.XPATH, f'//button[@aria-label="Go back to all teams"]').click()
+            time.sleep(60)
+
+
 login()
+selectClass()
---
+++ timetable.py
@@ -0,0 +1,25 @@
+
+os = "CSE 4A-T-CSB4218-OPERATING SYSTEMS"
+cn = "CSE4A-T-CSB4217-Computer Networks"
+maths = "CSE4A-T-MAA4219-Discrete Mathematics"
+coa = "CSE4A-T-CSB4216-COMPUTER ORGANIZATION AND ARCHITECTURE - 2022"
+dp = "CSE4A - P - CSB4243 - Design Project 1"
+nde = "IT-ITD4292-T-CYBER CRIME INVESTIGATION AND DIGITAL LAWS"
+de = "CSE4DE-T-CSC4278-FUNDAMENTALS OF CYBER SECURITY"
+osLab = "CSE 4A-L-CSB4242-OPERATING SYSTEMS LAB"
+cnLab = "CSE4A-P-CSB4241 Networking Lab"
+free = "free"
+brk = "break"
+
+
+timetable = {
+    "Monday": [maths, de, brk, nde, nde, free, osLab, osLab, osLab],
+    "Tuesday": [cn, maths, brk, coa, os, free, dp, dp, nde],
+    "Wednesday": [de, free, brk, cn, coa, free, cnLab, cnLab, cnLab],
+    "Thursday": [os, cn, brk, os, maths, free, coa, free, free],
+    "Friday": [os, maths, brk, maths, coa, free, de, de, free],
+    "Sunday": [cn, maths, brk, coa, os, free, dp, dp, nde],
+}
+
+tim = ["08.30", "09.20", "10.10", "10.30",
+       "11.20", "12.10", "13.00", "13.50", "14.40", "15.30"]
